cb391ead430ec23d310ba2145464732b32e186d5
Mathieu Acthernoene <zoontek@gmail.com>
2017-12-29 18:56:12 +0100

    Add a basic example project

---
+++ example/App.js
@@ -0,0 +1,39 @@
+// @flow
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+if (__DEV__) {
+  const DevMenu = require('react-native-dev-menu');
+  DevMenu.addItem('Say Hello', () => alert('Hello!'));
+}
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>Please open the developer menu</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
